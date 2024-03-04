3db63b077e04dcdcfa6bc5adfa57a775f956f53c
Jordan Burn <burnjordan@hotmail.com>
2024-03-04 16:45:08 +0100

    initial commit

---
+++ Assets/MaleAnim.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaleAnim : MonoBehaviour
+{
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.S))
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            anim.SetBool("isWalkingBackwards",true);
+            anim.SetBool("isWalkingBackwards", false);
+        }
+        
+        else
+        {
+            anim.SetBool("isRunning", false);
+            
+        }
+    }
+}
